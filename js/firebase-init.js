f84e90862d73735b5842dce7e557804ffc3b9e6f
nigel60913 <nigel60913@gmail.com>
2026-07-15 11:03:58 +0800

    Update firebase-init.js

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import {
-  getFirestore, collection, doc, getDoc, getDocs, setDoc, addDoc, updateDoc,
+  initializeFirestore, collection, doc, getDoc, getDocs, setDoc, addDoc, updateDoc,
   deleteDoc, onSnapshot, query, orderBy, serverTimestamp, writeBatch
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 import {
@@ -9,7 +9,10 @@ import {
 import { firebaseConfig, ADMIN_EMAILS } from "./firebase-config.js";
 
 export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+// 用長輪詢自動偵測：手機行動網路／某些校園或公司 WiFi 常會擋掉 Firestore 預設的
+// 即時連線方式，導致「不刷新頁面就不會自動換題」。這個設定會自動改用相容性更好的
+// 連線方式，修正這個問題。
+export const db = initializeFirestore(app, { experimentalAutoDetectLongPolling: true });
 export const auth = getAuth(app);
 
 export {
